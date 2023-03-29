98b126ba9314266d54904d8b5cc6906ec5ef697f
lequocmanhqn2022 <lehungmanhqn2018@gmail.com>
2023-03-29 20:40:49 +0700

    make api add_students

--- server/app/controller/studentsController.py
+++ server/app/controller/studentsController.py
@@ -23,36 +23,6 @@ def get_students():
 
     return jsonify(students_list)
 
-@api_students.route("/add_students/<int:account_id>", methods = ["POST"])
-def add_students(account_id):
-    check_user = session.query(Users).filter(Users.account_id == account_id).first()
-    if check_user:
-        return jsonify({'message': 'User already exists'})
-    
-    student = json.loads(request.form["student"])
-    lastName = student['lastName']
-    firstName = student['firstName']
-    email = student['email']
-    phone = student['phone']
-
-    avatar = request.files["avatar"]
-    filename = secure_filename(avatar.filename)
-    if not os.path.exists('app/static/images'):
-        os.makedirs('app/static/images')
-    avatar.save(os.path.join('app', 'static', 'images', filename))
-    images = url_for('static', filename=filename)
-
-    gender = student['gender']
-    birthDay = student['birthDay']
-    user = Users(account_id=account_id, lastName=lastName, firstName=firstName, email=email, phone=phone)
-    session.add(user)
-    session.commit()
-
-    students = Students(user_id=user.id, images=images, gender=gender, birthDay=birthDay)
-    session.add(students)
-    session.commit()
-    return jsonify({'message': 'Students added successfully'})
-
 @api_students.route("/get_student_byID/<int:user_id>", methods = ["GET"])
 def get_student_byID(user_id):
     student_db = session.query(Students).filter(Students.user_id == user_id).all()
@@ -64,3 +34,34 @@ def get_student_byID(user_id):
         students_list.append(students_dict)
 
     return jsonify(students_list)
+
+@api_students.route("/add_students/<int:account_id>", methods = ["POST"])
+def add_students(account_id):
+    check_user = session.query(Users).filter(Users.account_id == account_id).first()
+    if check_user:
+        return jsonify({'message': 'User already exists'})
+    
+    # add users
+    student = json.loads(request.form["student"])
+    lastName = student['lastName']
+    firstName = student['firstName']
+    email = student['email']
+    phone = student['phone']
+
+    user = Users(account_id=account_id, lastName=lastName, firstName=firstName, email=email, phone=phone)
+    session.add(user)
+    session.commit()
+    # add students
+    avatar = request.files["avatar"]
+    filename = secure_filename(avatar.filename)
+    if not os.path.exists('app/static/images'):
+        os.makedirs('app/static/images')
+    avatar.save(os.path.join('app', 'static', 'images', filename))
+    images = url_for('static', filename=filename)
+    gender = student['gender']
+    birthDay = student['birthDay']
+
+    students = Students(user_id=user.id, images=images, gender=gender, birthDay=birthDay)
+    session.add(students)
+    session.commit()
+    return jsonify({'message': 'Students added successfully'})
